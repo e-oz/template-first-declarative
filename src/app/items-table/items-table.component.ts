6e6f78f6bb329c5c4adeb08e301b65aba4b37a9f
Evgeniy OZ <normandiggs@gmail.com>
2024-02-04 16:54:43 +0100

    Step 2

--- src/app/items-table/items-table.component.ts
+++ src/app/items-table/items-table.component.ts
@@ -1,13 +1,25 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgTemplateOutlet } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import type { Item } from "../types";
+import { ItemsTableStore } from "./items-table.store";
 
 @Component({
   selector: 'items-table',
   standalone: true,
-  imports: [],
+  imports: [
+    NgTemplateOutlet
+  ],
   templateUrl: './items-table.component.html',
   styleUrl: './items-table.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ItemsTableStore],
 })
 export class ItemsTableComponent {
-  @Input({required: true}) items: any[] = [];
+  protected readonly store = inject(ItemsTableStore);
+
+  @Input({ required: true }) set items(items: Item[]) {
+    if (items) {
+      this.store.setItems(items);
+    }
+  }
 }
---
+++ src/app/items-table/items-table.store.ts
@@ -0,0 +1,56 @@
+import { computed, Injectable, signal } from "@angular/core";
+import type { Item } from "../types";
+
+export type ItemsTableHeader = {
+  path: keyof Item;
+  label: string;
+}
+
+@Injectable()
+export class ItemsTableStore {
+  readonly $headers = signal<ItemsTableHeader[]>([]);
+  readonly $sortByHeader = signal<keyof Item | undefined>(undefined);
+  readonly $sortingOrder = signal<'asc' | 'desc'>('asc');
+  readonly $itemsPerPage = signal<number>(5);
+  readonly $page = signal<number>(0);
+
+  private readonly $inputItems = signal<Item[]>([]);
+
+  readonly $sortedItems = computed(() => {
+    const sortPath = this.$sortByHeader();
+    if (!sortPath) {
+      return this.$inputItems();
+    }
+    const items = this.$inputItems().slice();
+
+    items.sort((a, b) => {
+      const aVal = a[sortPath];
+      const bVal = b[sortPath];
+      if (typeof aVal === 'number' && typeof bVal === 'number') {
+        return aVal < bVal ? -1 : (aVal > bVal ? 1 : 0);
+      } else {
+        return a[sortPath].toString().localeCompare(b[sortPath].toString());
+      }
+    });
+
+    if (this.$sortingOrder() === 'asc') {
+      return items;
+    } else {
+      return items.reverse();
+    }
+  });
+
+  readonly $items = computed(() => {
+    const page = this.$page();
+    const items = this.$sortedItems();
+    const perPage = this.$itemsPerPage();
+    if (page === 0 && items.length <= perPage) {
+      return items;
+    }
+    return items.slice(page * perPage, page * perPage + perPage);
+  });
+
+  setItems(items: Item[]) {
+    this.$inputItems.set(items.slice());
+  }
+}
